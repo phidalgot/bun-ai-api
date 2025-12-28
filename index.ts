f87b7fd491d9fb7830bd8390869414f5360f9d4c
midudev <miduga@gmail.com>
2025-12-28 22:14:49 +0100

    Add Cerebras and Groq SDK dependencies, implement service selection for chat functionality in the server, and update package.json and bun.lock accordingly.

--- index.ts
+++ index.ts
@@ -1,8 +1,45 @@
+import { groqService } from './services/groq';
+import { cerebrasService } from './services/cerebras';
+import type { AIService, ChatMessage } from './types';
+
+const services: AIService[] = [
+  groqService,
+  cerebrasService,
+  // Google Gemini
+  // OpenRouter
+  // otro servicio incluso local
+]
+let currentServiceIndex = 0;
+
+function getNextService() {
+  const service = services[currentServiceIndex];
+  currentServiceIndex = (currentServiceIndex + 1) % services.length;
+  return service;
+}
+
 const server = Bun.serve({
   port: process.env.PORT ?? 3000,
   async fetch(req) {
-    return new Response("API de Bun está funcionando correctamente");
+    const { pathname } = new URL(req.url)
+
+    if (req.method === 'POST' && pathname === '/chat') {
+      const { messages } = await req.json() as { messages: ChatMessage[] };
+      const service = getNextService();
+
+      console.log(`Using ${service?.name} service`);
+      const stream = await service?.chat(messages)
+
+      return new Response(stream, {
+        headers: {
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          'Connection': 'keep-alive',
+        },
+      });
+    }
+
+    return new Response("Not found", { status: 404 });
   }
 })
 
-console.log(`Server is running on ${server.url}:${server.port}`);
+console.log(`Server is running on ${server.url}`);
