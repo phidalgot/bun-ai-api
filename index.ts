fcb33d572aeb92f671717cc51b3152f499d8dc7c
Pedro Hidalgo <phidalgot@gmail.com>
2026-02-13 13:48:28 -0400

    api-bun-ia-ph

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import type { AIService, ChatMessage } from './types';
 
 const services: AIService[] = [
   groqService,
-  cerebrasService,
+  // cerebrasService,
   // Google Gemini
   // OpenRouter
   // otro servicio incluso local
@@ -18,12 +18,23 @@ function getNextService() {
 }
 
 const server = Bun.serve({
-  port: process.env.PORT ?? 3000,
+  port: process.env.PORT ?? 3003,
   async fetch(req) {
     const { pathname } = new URL(req.url)
 
+    if (req.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      });
+    }
+
     if (req.method === 'POST' && pathname === '/chat') {
       const { messages } = await req.json() as { messages: ChatMessage[] };
+      console.log('messages', messages)
       const service = getNextService();
 
       console.log(`Using ${service?.name} service`);
@@ -34,11 +45,15 @@ const server = Bun.serve({
           'Content-Type': 'text/event-stream',
           'Cache-Control': 'no-cache',
           'Connection': 'keep-alive',
+          'Access-Control-Allow-Origin': '*',
         },
       });
     }
 
-    return new Response("Not found", { status: 404 });
+    return new Response("Not found", {
+      status: 404,
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    });
   }
 })
 
